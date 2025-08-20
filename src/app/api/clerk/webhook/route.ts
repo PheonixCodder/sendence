c90ca5f5162433b64b3b87cc044fcea0599b4c39
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-08-20 17:52:20 +0500

    Integrated Clerk

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,20 @@
+import { db } from "@/server/db";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const { data } = await req.json();
+  console.log(data)
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.upsert({
+    where: { id },
+    update: { emailAddress, firstName, lastName, imageUrl },
+    create: { id, emailAddress, firstName, lastName, imageUrl },
+  });
+
+  return new NextResponse("Webhook received", { status: 200 });
+};
